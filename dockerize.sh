8de6c335f93782d698a96334b41c24d82f07b6fe
krzemLech <krzemmo@gmail.com>
2024-01-14 21:40:14 +0100

    removed prints

--- dockerize.sh
+++ dockerize.sh
@@ -2,6 +2,5 @@
 
 set -a
 . .env
-echo "DB_URI:" $DB_URI
 sudo docker build --tag flask-present .
-sudo docker run -p 5000:5000 flask-present
+sudo docker run -p 5001:5000 flask-present
--- flaskpresent/__init__.py
+++ flaskpresent/__init__.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 SECRET_KEY = os.getenv('SECRET_KEY')
 DB_URI = os.getenv('DB_URI')
 
-print('secret:', SECRET_KEY)
-print('db uri:', DB_URI)
-
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
 db = SQLAlchemy(app)
